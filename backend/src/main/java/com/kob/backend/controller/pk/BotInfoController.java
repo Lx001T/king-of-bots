f8f513f3fb885ae60bdea446fe303f9d50d312bc
Lx001T <2609193840@qq.com>
2024-04-12 14:26:49 +0800

    项目骨架搭建完成-前后端可以通信

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,20 +4,23 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<>();
-        list.add("ljq");
-        list.add("222");
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "ljq");
+        bot1.put("rating", "100");
 
-        return list;
+        return bot1;
+
     }
 
 }
