494ce5dc57afdb98709c2cb3876ed1cbfebe99fc
Lx001T <2609193840@qq.com>
2024-04-13 14:52:05 +0800

    pk页面地图绘制与障碍物实现

---
+++ web/src/components/PlayGround.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="playground">
+    <GameMap/>
+  </div>
+</template>
+
+<script>
+import GameMap from './GameMap.vue'
+
+export default {
+  components: {
+    GameMap,
+  }
+}
+
+</script>
+
+<style scoped>
+div.playground {
+  height: 70vh;
+  width: 60vw;
+  margin: 40px auto;
+}
+</style>
